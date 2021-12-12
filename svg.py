3f2aaecee7681907fcb04fdc3db949e9088806e5
Sayad Pervez <pervez2504@gmail.com>
2021-12-12 14:11:41 +0530

    Resize function added

--- svg.py
+++ svg.py
@@ -1,6 +1,17 @@
 import os
 import cairo
 
+def svgResize2Drawing(path):
+    if("./" in path):
+        pass
+    else:
+        if("/" in path):
+            p = path[:path.rindex("/")+1]
+            path = path[path.rindex("/")+1:]
+        else:
+            pass
+    os.system(f'inkscape --batch-process --actions="FitCanvasToDrawing;export-filename:{path};export-do;" {path}')
+
 def svgRotate(path,angle,outputFileName = "new"):
     if("./" in path):
         pass
@@ -10,7 +21,7 @@ def svgRotate(path,angle,outputFileName = "new"):
             path = path[path.rindex("/")+1:]
         else:
             pass
-    os.system(f'inkscape --batch-process --actions="select-all:all;transform-rotate:{str(angle)};FitCanvasToDrawing;export-filename:{outputFileName}.svg;export-do;" {path}')
+    os.system(f'inkscape --batch-process --actions="select-all:all;transform-rotate:{str(angle)};FitCanvasToDrawing;export-filename:{path};export-do;" {path}')
 
 def createCanvas(width,height):
     with cairo.SVGSurface("canvas.svg", width,height) as surface:
@@ -18,4 +29,4 @@ def createCanvas(width,height):
         context.scale(700, 700)
         #context.stroke()
 
-createCanvas(1920,1080)
+svgResize2Drawing("./new.svg")
